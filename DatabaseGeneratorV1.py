b246104a268203500c0bd1c463be26e8fe82954f
Jack4891 <95482991+Jack4891@users.noreply.github.com>
2024-02-17 09:27:35 -0500

    Added comments, throttled more
    When I mean Throttled more I mean I changed the amount of satellites it reads before pausing to 5 from 18 because I got another API violation

--- DatabaseGeneratorV1.py
+++ DatabaseGeneratorV1.py
@@ -1,11 +1,11 @@
 ##
 ## SLTrack.py
 ## (c) 2019 Andrew Stokes  All Rights Reserved
+## Edited by Jackson Andrew to better work with SWIFT-UDP research project
 ##
-##
-## Simple Python app to extract Starlink satellite history data from www.space-track.org into a spreadsheet
+## Simple Python app to extract Iridium satellite latest data from www.space-track.org into a text file
 ## (Note action for you in the code below, to set up a config file with your access and output details)
-##
+## 
 ##
 ##  Copyright Notice:
 ##
@@ -38,11 +38,14 @@ class MyError(Exception):
 # See https://www.space-track.org/documentation for details on REST queries
 # the "Find Starlinks" query searches all satellites with NORAD_CAT_ID > 40000, with OBJECT_NAME matching STARLINK*, 1 line per sat
 # the "OMM Starlink" query gets all Orbital Mean-Elements Messages (OMM) for a specific NORAD_CAT_ID in JSON format
+# (Jackson) leaving above comment in in case readers curious how queries were made 
+# (Jackson) current query was edited from a suggested query by Space Track admim 
+        # The reason for editing it is because the query output an HTML which wasn't useful to us     
 
 uriBase                = "https://www.space-track.org"
 requestLogin           = "/ajaxauth/login"
 requestCmdAction       = "/basicspacedata/query" 
-requestFindStarlinks   = "/class/gp/GP_ID/%3E/OBJECT_TYPE/payload/orderby/GP_ID%20desc/format/json/OBJECT_NAME/iridium~~/"
+requestFindIridiums   = "/class/gp/GP_ID/%3E/OBJECT_TYPE/payload/orderby/GP_ID%20desc/format/json/OBJECT_NAME/iridium~~/"
 requestOMMStarlink1    = "/class/omm/NORAD_CAT_ID/"
 requestOMMStarlink2    = "/orderby/EPOCH%20asc/format/limit/1/json"
 
@@ -62,8 +65,12 @@ TPI86 = 2.0 * PI / 86400.0
 #=========================
 # Provide a config file in the same directory as this file, called SLTrack.ini, with this format (without the # signs)
 # [configuration]
+
+# (Jackson) this is done it is already in SWIFT-UDP server with below information
+# there is a reduntant add on with the output but I'm leaving it in case I need to debug with the source code
+
 # username = "ajackson03@vt.edu"
-# password = "IdowhatIwant1!!!"
+# password = let me know if you need the password I use the same one for almost all VT related logins
 # output = "STLtrack.xlsx"
 #
 # ... where XXX and YYY are your www.space-track.org credentials (https://www.space-track.org/auth/createAccount for free account)
@@ -78,14 +85,14 @@ configOut = config.get("configuration","output")
 siteCred = {'identity': configUsr, 'password': configPwd}
 
 
- 
+ # (Jackson) This is an add on I made such that it runs continuously
 while True:
     # use requests package to drive the RESTful session with space-track.org
         # get a time variable
     now = datetime.now()
     nowStr = now.strftime("%m/%d/%Y %H:%M:%S")
 
-    # (Jackson) open text file
+    # (Jackson) open text file and write current time to it for debugging purposes
     f = open("sltrack_iridium.txt", "w")
     f.write(nowStr + "\n")
 
@@ -98,21 +105,28 @@ while True:
             raise MyError(resp, "POST fail on login")
 
         # this query picks up all Starlink satellites from the catalog. Note - a 401 failure shows you have bad credentials 
-        resp = session.get(uriBase + requestCmdAction + requestFindStarlinks)
+        # (Jackson) needs to get the list of all iridium satellites to later loop through 
+        resp = session.get(uriBase + requestCmdAction + requestFindIridiums)
         if resp.status_code != 200:
             print(resp)
             raise MyError(resp, "GET fail on request for Starlink satellites")
 
         # use the json package to break the json formatted response text into a Python structure (a list of dictionaries)
+        # (Jackson) parses above command to get loop ready
         retData = json.loads(resp.text)
         satCount = len(retData)
         satIds = []
+
+        # (Jackson) The start of the loop
+        # Loop uses e as the pointer to each omm of that satellite gained from the original search
+        # builds an array of NORAD_CA_IDs to be later used in getting specific data
         for e in retData:
             # each e describes the latest elements for one Starlink satellite. We just need the NORAD_CAT_ID 
             catId = e['NORAD_CAT_ID']
             satIds.append(catId)
 
-        # using our new list of Starlink satellite NORAD_CAT_IDs, we can now get the OMM message
+        # using our new list of Starlink Iridium NORAD_CAT_IDs, we can now get the OMM message
+        # (Jackson) This is another loop that uses the array of NORAD_CAT_IDs to get specific data
         maxs = 1
         for s in satIds:
             resp = session.get(uriBase + requestCmdAction + requestOMMStarlink1 + s + requestOMMStarlink2)
@@ -120,26 +134,31 @@ while True:
                     # If you are getting error 500's here, its probably the rate throttle on the site (20/min and 200/hr)
                     # wait a while and retry
                     print(resp)
-                    raise MyError(resp, "GET fail on request for Starlink satellite " + s)
+                    raise MyError(resp, "GET fail on request for Iridum satellite " + s)
 
-                # the data here can be quite large, as it's all the elements for every entry for one Starlink satellite
+                # the data here can be quite large, as it's all the elements for every entry for one Iridium satellite
+                # (TO DO) need to find a better query that just gets the latest OMM 
             retData = json.loads(resp.text)
-            # each element is one reading of the orbital elements for one Starlink
             e = retData[-1]
             print("Scanning satellite " + e['OBJECT_NAME'] + " at epoch " + e['EPOCH'])
-
+            # Writes TLE data to the text file
             f.write(e['TLE_LINE0'] + "\n")
             f.write(e['TLE_LINE1'] + "\n")
             f.write(e['TLE_LINE2'] + "\n") 
             
             maxs = maxs + 1
-            if maxs > 18:
+            # (Jackson) This is a change from the original program where I changed the if statement from 
+            # comparing 18 to comparing 5 because of getting lots of API violations
+            # I think that because Starlink satellites don't have as much history this wasn't a problem
+            # with starlink satellites but Iridium has been around longer which causes issues
+            if maxs > 5:
                     
                     print("Snoozing for 60 secs for rate limit reasons (max 20/min and 200/hr)...")
                     time.sleep(60)
                     maxs = 1
         session.close()
-    
+    # (Jackson) This closes the text file 
+         # important as a bug fix where check it after the code had done 1 loop some later data didn't save
     f.close
     
     print("Completed session") 
